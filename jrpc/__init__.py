27d9654d46db2c74f23d1d31e578412841efd675
Alex Sherman <asherman1024@gmail.com>
2014-11-23 14:55:11 -0600

    Added author information

--- jrpc/__init__.py
+++ jrpc/__init__.py
@@ -1,3 +1,10 @@
 __all__ = ["service", "exceptions"]
 import service
 import exception
+
+__author__ = "Alex Sherman <asherman1024@gmail.com>"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Alex Sherman", "Peter Den Hartog"]
+__license__ = "MIT"
+__maintainer__ = "Alex Sherman"
+__email__ = "asherman1024@gmail.com"
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from distutils.core import setup
 
-setup(name = "jrpc", version = "1.0", packages = ["jrpc"])
+setup(
+  name = "jrpc",
+  version = "1.0",
+  packages = ["jrpc"],
+  author='Alex Sherman',
+  author_email='asherman1024@gmail.com',
+  url='https://github.com/alex-sherman/python-jrpc')
