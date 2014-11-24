615c547e262ece6164bc6dc757483c9d8f01fa40
Low Kian Seong <kianseong@gmail.com>
2014-11-24 09:11:34 +0800

    Update setup.py
    Adding `description` field in `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
   name = "python-jrpc",
   version = "1.1",
+  description = "A Python remote procedure call framework that uses JSON RPC v2.0",
   packages = ["jrpc"],
   author='Alex Sherman',
   author_email='asherman1024@gmail.com',
