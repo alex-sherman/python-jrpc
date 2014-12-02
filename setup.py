8dd351bc8bff59c5844193cafa4d32e761252a8f
Alex Sherman <asherman1024@gmail.com>
2014-12-02 12:49:15 -0600

    Incremented version number to 1.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = "python-jrpc",
-  version = "1.1.1",
+  version = "1.1.2",
   description = "A Python remote procedure call framework that uses JSON RPC v2.0",
   packages = ["jrpc"],
   author='Alex Sherman',
